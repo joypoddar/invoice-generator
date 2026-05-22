e44fa0347c36bd75ddde2ea9a0e3dc75ef3c08d3
Joy Poddar <joypoddar.sd@gmail.com>
2026-05-22 18:24:52 +0530

    feat: add date and currency formatting utilities with comprehensive test cases

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,13 @@
+export {
+  renderInvoiceHtml,
+  type BrandingOpts,
+  type RenderOpts,
+} from './invoice-html.js';
+export {
+  formatCurrency,
+  formatCurrencyMaybeInt,
+  formatDate,
+  formatPercent,
+  DEFAULT_DATE_FORMAT,
+  type DateFormat,
+} from './format.js';
